2c6b5986ff9eeb662171b3d4469606836468eaf3
Rick den Otter <rickdotyt@gmail.com>
2023-02-22 19:41:47 +0100

    BAckground subtraction almost finished

--- assignments/2_background_subtraction.py
+++ assignments/2_background_subtraction.py
@@ -19,7 +19,7 @@ for cam in range(1, 5):
         else:
             vid.release()
             break
-    cv.imshow('bg', background_subtractor.getBackgroundImage())
+    # cv.imshow('bg', background_subtractor.getBackgroundImage())
 
     vid = cv.VideoCapture(video_path)
     while vid.isOpened():
@@ -28,10 +28,17 @@ for cam in range(1, 5):
             fg = background_subtractor.apply(frame, learningRate=0)
             # Remove detected shadows
             fg[fg == 127] = 0
-            fg = cv.erode(fg, erosion_kernel)
 
-            cv.imshow('video', frame)
-            cv.imshow('fg', fg)
+            # fg = cv.erode(fg, erosion_kernel, iterations=1)
+            # fg = cv.dilate(fg, erosion_kernel, iterations=1)
+            contours, hierarchy = cv.findContours(fg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            biggest_contour = max(contours, key=cv.contourArea)
+
+            mask = np.zeros(fg.shape, np.uint8)
+            # Perhaps dilate or erode? Better to overestimate area so dilate?
+            cv.drawContours(mask, [biggest_contour], 0, 255, -1)
+            cv.imshow('image', frame)
+            cv.imshow('fg', mask)
             cv.waitKey(0)
         else:
             vid.release()
